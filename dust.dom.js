ad3b386ca663e494130472eacaad340cdfc51b70
Bob Fanger <bfanger@gmail.com>
2013-01-19 16:57:58 +0100

    Rewrote domHook & renderDom to compensate for asynchronous rendering.

--- dust.dom.js
+++ dust.dom.js
@@ -1,26 +1,29 @@
 (function (dust) {
 	"use strict";
 
+	/**
+	 * Create a temporary data-attribute and call the callback when the template is converted to HTMLElements.
+	 * Requires that the template is rendered with dust.renderDom()
+	 *
+	 * @param  {Function} callback  function ({HTMLElement}) { ... }
+	 * @return {string} Example: 'data-dust-dom="hook123"'
+	 */
 	dust.domHook = function(callback) {
 		dust.domHook.counter++;
-		if (typeof dust.domHook.callbacks[dust.domHook.index] === 'undefined') {
-			console.warn("dust.domHook() doesn't work with `template(vars);` use `dust.render(template, vars);`");
-			return '';
-		}
-		dust.domHook.callbacks[dust.domHook.index][dust.domHook.counter] = callback;
-		return dust.domHook.attribute + dust.domHook.counter + '="domHook"';
+		dust.domHook.callbacks['hook' + dust.domHook.counter] = callback;
+		return dust.domHook.attribute + '="hook' + dust.domHook.counter + '"';
 	};
 	dust.domHook.attribute = 'data-dust-dom';
 	dust.domHook.counter = 0;
-	dust.domHook.callbacks = [];
-	dust.domHook.index = -1;
+	dust.domHook.callbacks = {};
 
 	/**
-	 * Create an element placeholder in the html, the
+	 * Create an element placeholder in the html and call the callback when the template is converted to HTMLElements.
+	 * Requires that the template is rendered with dust.renderDom()
 	 *
-	 * @param {string} tagName  Specifies the type of element to be created. Example: "div"
-	 * @param {type} callback
-	 * @returns {string}
+	 * @param {string} tagName  Specifies the type of element to be created. Example: "div" or "span"
+	 * @param {Function} callback  function ({HTMLElement}) { ... }
+	 * @returns {string} Example: '<div data-dust-dom="hook123"></div>'
 	 */
 	dust.createElement = function(tagName, callback) {
 		return '<' + tagName + ' ' + dust.domHook(callback) + '></' + tagName +'>';
@@ -28,31 +31,30 @@
 
 	/**
 	 * Render the template to a DOM NodeList.
+	 * Enables helpers that use dust.domHook() or dust.createElement()
 	 *
 	 * @param {string} name  The name of the template.
 	 * @param {object} context  The context for the template.
-	 * @param {HTMLElement} element (optional)
-	 * @param callback  function (NodeList, err) { ... }
+	 * @param callback  function (err, {NodeList} out) { ... }
 	 */
 	dust.renderDom = function (name, context, callback) {
-		dust.domHook.index++;
-		var callbacks = this.domHook.callbacks[this.domHook.index] = {};
-
 		this.render(name, context, function (err, html) {
 			if (err) {
 				return callback(err, html);
 			}
+			// Convert html to DOM elements
 			var container = document.createElement('div');
 			container.innerHTML = html;
 			// Process created callbacks
-			for (var i in callbacks) {
-				var selector = '[' + dust.domHook.attribute + i + ']';
-				var element = container.querySelector(selector);
-				element.removeAttribute(dust.domHook.attribute + i);
-				callbacks[i](element);
+			var elements = container.querySelectorAll('[' + dust.domHook.attribute + ']');
+			for (var i = 0; i < elements.length; i++) {
+				var element = elements[i];
+				console.dir(element);
+				var hook = element.getAttribute(dust.domHook.attribute);
+				element.removeAttribute(dust.domHook.attribute);
+				dust.domHook.callbacks[hook](element);
+				delete dust.domHook.callbacks[hook];
 			}
-			delete dust.domHook.callbacks[dust.domHook.index];
-			//this.domHook.index--;
 			callback(err, container.childNodes);
 		});
 	};
